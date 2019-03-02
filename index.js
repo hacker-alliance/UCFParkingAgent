c59f842e903aa57fb0c923b31d4465ef69b8590d
nayoshi12 <nayoshi12@gmail.com>
2019-03-02 10:59:54 -0500

    This is where Matt breaks everything

--- index.js
+++ index.js
@@ -36,19 +36,36 @@ restService.use(
 
 restService.use(bodyParser.json());
 
+// restService.post("/garage", function(req, res) {
+//   var garageAvail = [];
+//
+//   return request(url, (function (error, response, body) {
+//   	const $ = cheerio.load(body);
+//
+//     // Parse and save garage parking spaces number
+//   	$('strong').each(function(i, elem) {
+//   		//Get Garage Load
+//   		garageAvail[i] = $(this).text();
+//   	});
+//
+//     var garage_name = req.body.queryResult.parameters.garage;
+//
+//     return res.json({
+//       "fulfillmentText": garageAvail[garages[garage_name]] + "/" + garage_capacity[garage_name],
+//       "payload": {
+//         "google": {
+//           "expectUserResponse": true
+//         }
+//       }
+//     });
+//   }));
+// });
 restService.post("/garage", function(req, res) {
   var garageAvail = [];
+  scrape_garage().then(function(garageJSON){
+    console.log(garageJSON);
+  })
 
-  return request(url, (function (error, response, body) {
-  	const $ = cheerio.load(body);
-
-    // Parse and save garage parking spaces number
-  	$('strong').each(function(i, elem) {
-  		//Get Garage Load
-  		garageAvail[i] = $(this).text();
-  	});
-
-    var garage_name = req.body.queryResult.parameters.garage;
 
     return res.json({
       "fulfillmentText": garageAvail[garages[garage_name]] + "/" + garage_capacity[garage_name],
@@ -60,7 +77,24 @@ restService.post("/garage", function(req, res) {
     });
   }));
 });
+function scrape_garage(){
+  var garageAvail = [];
+  return new Promise((resolve,reject)=>{
+    request(url, (function (error, response, body) {
+    	const $ = cheerio.load(body);
 
+      // Parse and save garage parking spaces number
+      	$('strong').each(function(i, elem) {
+      		//Get Garage Load
+      		garageAvail[i] = $(this).text();
+        });
+        resolve(garageAvail);
+    	});
+
+
+
+    })
+}
 restService.listen(process.env.PORT || 8000, function() {
   console.log("Server up and listening");
 });
