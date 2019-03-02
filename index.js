d42fdeaa4c1253bb5dc49d85df9636166543db7d
Blake Wyatt <nabakin@protonmail.ch>
2019-03-02 06:49:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -53,7 +53,9 @@ restService.post("/garage", function(req, res) {
     : "Seems like some problem. Speak again.";*/
 
   return res.json({
-    "speech": req.body.queryResult.parameters.garage
+    "speech": req.body.queryResult.parameters.garage,
+    "displayText": req.body.queryResult.parameters.garage,
+    "source": "webhook-echo-sample"
   });
 });
 
