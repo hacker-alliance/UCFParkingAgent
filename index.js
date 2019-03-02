ddd71527142c318348f87cd6a735a6ab2dd33793
Blake Wyatt <nabakin@protonmail.ch>
2019-03-02 08:35:09 -0500

    Update index.js

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ restService.post("/garage", function(req, res) {
   });
 
   return res.json({
-    "fulfillmentText": garageAvail[garages[req.body.queryResult.parameters.garage]]
+    "fulfillmentText": "test"
   });
 });
 
