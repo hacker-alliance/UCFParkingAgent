33a9a9b45834d739f2d91d5d716b31b2dfcff8c3
unknown <nayoshi12@gmail.com>
2019-03-11 02:17:52 -0400

    Testing suggestions

--- index.js
+++ index.js
@@ -19,27 +19,60 @@ restService.use(
 );
 restService.use(bodyParser.json());
 
-function addSuggestions(conv){
-  for(i =0;i<suggestion.length;i++){
-    // console.log(suggestions[i]);
-    agent.add(new Suggestions(suggestions[i]));
+var flavortextSpotsTaken = {
+  0: function(garage, count, total){
+    return "<speak>In " + garage + ", there are " + subAlias(count) + " cars parked out of " + subAlias(total)+"</speak>";
+  },
+  1: function(garage, count, total){
+    return "<speak>There are " + count.toString() + " cars out of " + subAlias(total) + " in garage " + garage+"</speak>" ;
+  },
+  2: function(garage, count, total){
+    return "<speak>Garage " + garage + " is " + Math.round(Math.min(100, Math.max(0, (count/total)*100))).toString() + "% full</speak>";
   }
 }
+
 function addsuggestions(conv){
   for(i=0;i<suggestions.length;i++){
     conv.ask(new Suggestions(suggestions[i]));
   }
 }
 app.intent('Default Welcome Intent', conv => {
-  conv.ask('Hi, how is it going?')
-  conv.ask(`Here's a picture of a cat`)
-  conv.ask(new Image({
-    url: 'https://developers.google.com/web/fundamentals/accessibility/semantics-builtin/imgs/160204193356-01-cat-500.jpg',
-    alt: 'A cat',
-  }))
+  conv.ask('Hi, how is it going?');
   addsuggestions(conv);
-
 })
+app.intent('Spots Taken Intent',conv =>{
+  const garageLetter = agent.parameters.garage;
+  let scrapedata = await(async ()=>{
+    let jsondata = await scraper();
+    console.log(jsondata);
+    let response = flavortextSpotsTaken[getRandomInt(3)](garageLetter,Math.max(0, garage_capacity[garageLetter]-jsondata[garages[garageLetter]]),garage_capacity[garageLetter]);
+    conv.ask(new SimpleResponse({
+        // <speak></speak> is needed here since factPrefix is a SSML string
+        // and contains audio.
+        speech: response,
+        text: "TESTTTTT",
+      }));
+  })();
+  addSuggestions(conv);
+})
+function spotsTaken(agent){
+  const garageLetter = agent.parameters.garage;
+  let scrapedata = await(async ()=>{
+    let jsondata = await scraper();
+    console.log(jsondata);
+    let response = flavortextSpotsTaken[getRandomInt(3)](garageLetter,Math.max(0, garage_capacity[garageLetter]-jsondata[garages[garageLetter]]),garage_capacity[garageLetter]);
+
+     agent.add(new Text({
+      text: response,
+      platform: "ACTIONS_ON_GOOGLE"
+    }));
+  })();
+
+  addSuggestions(agent);
+
+  return agent;
+}
+
 
 
 restService.post("/garage", app);
