2300937dacc2907a40d19f13fbb2216a1fa70073
nayoshi12 <nayoshi12@gmail.com>
2019-03-26 13:03:11 -0400

    Added support with moment timezone to see how it works

--- intents/WelcomeIntent.js
+++ intents/WelcomeIntent.js
@@ -7,7 +7,7 @@ module.exports = async function(conv,lib){
 
   let flavorNumber = getRandomInt(flavortext[intent].length);
   let time = moment();
-  console.log(time.format("MMMM Do YYYY, h:mm:ss a"));
+  console.log(time.tz("America/New_York").format("MMMM Do YYYY, h:mm:ss a"));
   conv.ask(new SimpleResponse({
     text:flavortext[intent][flavorNumber].text,
     speech:flavortext[intent][flavorNumber].speech
