cc16f8ca52f9e5c46d9a30b9e3101a2d2c843e5f
nayoshi12 <nayoshi12@gmail.com>
2019-03-26 12:57:19 -0400

    Added Welcome Intent console message to determine if it's time dependant

--- intents/WelcomeIntent.js
+++ intents/WelcomeIntent.js
@@ -2,16 +2,17 @@ module.exports = async function(conv,lib){
   let intent = conv.intent;
   console.log("Running: " + conv.intent);
 
-  const {Suggestions,helper,SimpleResponse} = lib;
+  const {Suggestions,helper,SimpleResponse,moment} = lib;
   const {addSuggestions,flavortext,getRandomInt} = helper;
 
   let flavorNumber = getRandomInt(flavortext[intent].length);
-
+  let time = moment();
+  console.log(time.format("MMMM Do YYYY, h:mm:ss a"));
   conv.ask(new SimpleResponse({
     text:flavortext[intent][flavorNumber].text,
     speech:flavortext[intent][flavorNumber].speech
   }));
-  
+
   addSuggestions(conv,Suggestions);
   return conv;
 }
